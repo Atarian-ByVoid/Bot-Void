4740066f6505e1b2efcc61b803201862c4981f43
Carlos Alberto <cgomespimentel7@gmail.com>
2023-03-05 13:20:35 -0300

    New Version

--- bot.py
+++ bot.py
@@ -1,45 +1,38 @@
 import telebot 
-import requests
 
 KEY_API="5960212647:AAGTUduWXvTlTQ8STfkW9nzmvFLM9veGYy8"
 bot = telebot.TeleBot(KEY_API)
 
 
-@bot.message_handler(commands=["test1"])
-def test1(message): 
-      pass
-@bot.message_handler(commands=["test2"])
-def test2(message): 
-      pass
-@bot.message_handler(commands=["test3"])
-def test3(message): 
-      pass
+@bot.message_handler(commands=["division1"])
+def division1(message): 
+      bot.send_message(message.chat.id, "The Division Of The Dark")
+@bot.message_handler(commands=["division2"])
+def division2(message): 
+      bot.send_message(message.chat.id, "The Division Of The Light")
+@bot.message_handler(commands=["division3"])
+def division3(message): 
+      bot.send_message(message.chat.id, "The Division Of The Unknown")
+
             
-@bot.message_handler(commands=["opcao1"])
+@bot.message_handler(commands=["option1"])
 def option1(message): 
       text = """  
       Escolha um caminho do vazio para seguir
-      /test1 Teste1
-      /test2 Teste2
-      /test3 Teste3"""
+      /division1 Division Path 1
+      /division2 Division Path 2
+      /division3 Division Path 3"""
       bot.message(message.chat.id, text)
 
-
-
-@bot.message_handler(commands=["opcao2"])
+@bot.message_handler(commands=["option2"])
 def option2(message): 
-      pass
+      bot.send_message(message.chat.id,"New Wave")
 
-@bot.message_handler(commands=["opcao3"])
+@bot.message_handler(commands=["option3"])
 def option3(message): 
       print(message)
       bot.send_message(message.chat.id,"The Division Of The Void")
       
-@bot.message_handler(commands=["opcao4"])
-def option4(message): 
-      bot.send_photo(message=message, photo=open('images\Sunny.jpg', 'rb'))
-
-
 
 #Vai sempre retornar a mesma messnagem independente da pergunta 
 def verify(message):
@@ -51,11 +44,9 @@ def replied(message):
     text = """
         Hi, welcome to the void
         Escolha uma opção para continuar(clique no item):
-        /opcao1 teste 1
-        /opcao2 teste 2
-        /opcao3 teste 3
-        /opcao4 teste 4
-
+        /option1 teste 1
+        /option2 teste 2
+        /option3 teste 3
         Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
 
     bot.reply_to(message,text)
