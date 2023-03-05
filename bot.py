dfadbe3a6405516b677ec9417b7c116fefd74b15
Carlos Alberto <cgomespimentel7@gmail.com>
2023-03-05 13:02:16 -0300

    New Options

--- bot.py
+++ bot.py
@@ -1,15 +1,30 @@
 import telebot 
-
+import requests
 
 KEY_API="5960212647:AAGTUduWXvTlTQ8STfkW9nzmvFLM9veGYy8"
 bot = telebot.TeleBot(KEY_API)
 
 
-
-
+@bot.message_handler(commands=["test1"])
+def test1(message): 
+      pass
+@bot.message_handler(commands=["test2"])
+def test2(message): 
+      pass
+@bot.message_handler(commands=["test3"])
+def test3(message): 
+      pass
+            
 @bot.message_handler(commands=["opcao1"])
 def option1(message): 
-      pass
+      text = """  
+      Escolha um caminho do vazio para seguir
+      /test1 Teste1
+      /test2 Teste2
+      /test3 Teste3"""
+      bot.message(message.chat.id, text)
+
+
 
 @bot.message_handler(commands=["opcao2"])
 def option2(message): 
@@ -17,14 +32,12 @@ def option2(message):
 
 @bot.message_handler(commands=["opcao3"])
 def option3(message): 
-      bot.send_photo()
-
-
-
-
-
-
-
+      print(message)
+      bot.send_message(message.chat.id,"The Division Of The Void")
+      
+@bot.message_handler(commands=["opcao4"])
+def option4(message): 
+      bot.send_photo(message=message, photo=open('images\Sunny.jpg', 'rb'))
 
 
 
@@ -41,19 +54,11 @@ def replied(message):
         /opcao1 teste 1
         /opcao2 teste 2
         /opcao3 teste 3
+        /opcao4 teste 4
+
         Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
 
-
-
-
-
     bot.reply_to(message,text)
-
-
-
-
-
-
 
 #Entra em looping
 bot.polling()
